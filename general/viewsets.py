76ee402a0910adaf737363bf8cb3ed261921d657
Josue Ramos <nullrootbyte@gmail.com>
2024-10-17 01:03:19 -0600

    fix: viewsets

--- general/viewsets.py
+++ general/viewsets.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from general.serializers import *
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
     permission_classes = [IsAuthenticated]
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        return User.objects.filter(id=self.request.user.id)
+    def list(self, request):
+        queryset = User.objects.get(pk = request.user.id)
+        serializer = UserSerializer(queryset)
+        return Response(serializer.data)
